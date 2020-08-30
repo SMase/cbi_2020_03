53b08c42655ecf436ff5d2b1f33cf6934e34b1b9
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-08-31 02:09:56 +0900

    refactored

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pandas as pd
-import argparse, itertools
+import argparse, itertools, os
 from saliency import VanillaGrad
 
 def yyplot(y_obs, y_pred):
@@ -24,122 +24,122 @@ def yyplot(y_obs, y_pred):
     plt.tick_params(labelsize=16)
     plt.savefig('figure.png')
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ngpu", '-g', help="number of gpu", type=int, default=1)
-parser.add_argument("--batch_size", '-B', help="batch_size", type=int, default=8)
-parser.add_argument("--num_workers", help="number of workers", type=int, default=7)
-parser.add_argument("--n_graph_layer", help="number of GNN layer", type=int, default=4)
-parser.add_argument("--d_graph_layer", help="dimension of GNN layer", type=int, default=140)
-parser.add_argument("--n_FC_layer", help="number of FC layer", type=int, default=4)
-parser.add_argument("--d_FC_layer", help="dimension of FC layer", type=int, default=128)
-parser.add_argument("--data_fpath", '-d', help="file path of dude data", type=str, default='data_diff/')
-parser.add_argument("--save_dir", '-S', help="save directory of model parameter", type=str, default='./save/')
-parser.add_argument("--initial_mu", help="initial value of mu", type=float, default=4.461085466198279)
-parser.add_argument("--initial_dev", help="initial value of dev", type=float, default=0.19818493842903845)
-parser.add_argument("--dropout_rate", '-D', help="dropout_rate", type=float, default=0.3)
-parser.add_argument("--test_keys", '-t', help="test keys", type=str, default='keys/test_keys.txt')
-parser.add_argument("--map_path", '-P', help="savepath for saliency map", type=str, default='')
-parser.add_argument("--map_th_max", help="max th value for saliency map", type=int, default=20)
-parser.add_argument("--map_th_min", help="min th value for saliency map", type=int, default=-20)
-parser.add_argument('--dataset_version', '-v', help='dataset version', type=int, default=2)
-args = parser.parse_args()
-if args.dataset_version == 1:
-    import dataset as ds
-elif args.dataset_version == 2:
-    import dataset2 as ds
-else:
-    raise Exception('Wrong dataset version')
+def read_keyfile(fname, local=False):
+    keys = []
+    for line in open(fname):
+        it = line.rstrip().split('\t')
+        if local:
+            pdb_code, value = it[0], float(it[1])
+        else:
+            pdb_code, ligand_name, year, value = it[0], it[1], int(it[2]), float(it[3])
+        if os.path.exists(f'cbidata/{pdb_code}'):
+            keys.append((pdb_code, value))
+    return keys
 
-print(args)
+def test(model, dataloader):
+    test_true = []
+    test_pred = []
+    test_label = []
 
-prm = edict()
-prm["n_graph_layer"] = args.n_graph_layer
-prm["d_graph_layer"] = args.d_graph_layer
-prm["n_FC_layer"] = args.n_FC_layer
-prm["d_FC_layer"] = args.d_FC_layer
-prm["dropout_rate"] = args.dropout_rate
-prm["initial_mu"] = args.initial_mu
-prm["initial_dev"] = args.initial_dev
+    saliency_list = []
+    n_atom_list = []
 
-test_keys = []
-test_pkd = []
-### Original
-# for line in open(args.test_keys):
-#     it = line.rstrip().split('\t')
-#     pdb_code, ligand_name, year, value = it[0], it[1], int(it[2]), float(it[3])
-#     test_keys.append(pdb_code)
-#     test_pkd.append(value)
+    model.eval()
+    for i_batch, sample in enumerate(dataloader):
+        model.zero_grad()
+        H, A1, A2, Y, V, keys, n_atom = sample
 
-### Stratifying
-for line in open(args.test_keys):
-    it = line.rstrip().split('\t')
-    pdb_code, value = it[0], float(it[1])
-    test_keys.append(pdb_code)
-    test_pkd.append(value)
+        #train neural network
+        embed = model.embede(H)
+        model.zero_grad()
+        pred = model.test_model((embed, A1, A2, V))
 
-dataset_path = args.data_fpath
-test_dataset = ds.MolDataset(test_keys, test_pkd, dataset_path)
+        out = torch.sum(pred)
+        embed.retain_grad()
+        out.backward()
+        saliency = embed.grad.clone()
+        saliency *= embed.data.clone()
 
-N_atom_features = test_dataset[0]['H'].shape[1]//2
-args.N_atom_features = N_atom_features
+        #collect loss, true label and predicted label
+        test_true.append(Y.data.cpu().numpy())
+        test_pred.append(pred.data.cpu().numpy())
+        test_label.append(keys)
+        saliency_list.append(saliency)
+        n_atom_list.append(n_atom)
 
-prm['N_atom_features'] = args.N_atom_features
+    test_pred = np.concatenate(np.array(test_pred), 0)
+    test_true = np.concatenate(np.array(test_true), 0)
+    return test_true, test_pred, test_label
 
-model_path = args.save_dir + "model_weights.pt"
-model = gnn(prm)
-model.load_state_dict(torch.load(model_path))
+def main(args):
+    prm = edict()
+    for n in ['n_graph_layer', 'd_graph_layer', 'n_FC_layer', 'd_FC_layer',
+              'dropout_rate', 'initial_mu', 'initial_dev']:
+        prm[n] = getattr(args, n)
 
-test_dataloader = DataLoader(test_dataset, 8, shuffle=True, num_workers=8, collate_fn=ds.collate_fn)
+    test_keys = read_keyfile(args.test_keys, local=True)
 
-test_true = []
-test_pred = []
-test_label = []
+    dataset_path = args.data_fpath
+    test_dataset = ds.MolDataset([k for k, v in test_keys], [v for k, v in test_keys], dataset_path)
 
-saliency_list = []
-n_atom_list = []
+    N_atom_features = test_dataset[0]['H'].shape[1]//2
+    args.N_atom_features = N_atom_features
 
-model.eval()
-for i_batch, sample in enumerate(test_dataloader):
-    model.zero_grad()
-    H, A1, A2, Y, V, keys, n_atom = sample
+    prm['N_atom_features'] = args.N_atom_features
 
-    #train neural network
-    embed = model.embede(H)
-    model.zero_grad()
-    pred = model.test_model((embed, A1, A2, V))
+    model_path = f'{args.save_dir}/model_weights.pt'
+    model = gnn(prm)
+    model.load_state_dict(torch.load(model_path))
 
-    out = torch.sum(pred)
-    embed.retain_grad()
-    out.backward()
-    saliency = embed.grad.clone()
-    saliency *= embed.data.clone()
+    test_dataloader = DataLoader(test_dataset, 8, shuffle=True, num_workers=8, collate_fn=ds.collate_fn)
+    test_true, test_pred, test_label = test(model, test_dataloader)
 
-    #collect loss, true label and predicted label
-    test_true.append(Y.data.cpu().numpy())
-    test_pred.append(pred.data.cpu().numpy())
-    test_label.append(keys)
-    saliency_list.append(saliency)
-    n_atom_list.append(n_atom)
+    rmse = mean_squared_error(test_true, test_pred)**0.5
+    mae = mean_absolute_error(test_true, test_pred)
+    r2_p = stats.pearsonr(test_true, test_pred)
+    spearman_r = stats.spearmanr(test_true, test_pred)
 
-test_pred = np.concatenate(np.array(test_pred), 0)
-test_true = np.concatenate(np.array(test_true), 0)
+    print(f'rmse: {rmse:5.3f}')
+    print(f'mae: {mae:5.3f}')
+    print(f'r2: {r2_p[0]:5.3f}')
+    print(f'rho: {spearman_r[0]:5.3f}')
 
-rmse = mean_squared_error(test_true, test_pred)**0.5
-mae = mean_absolute_error(test_true, test_pred)
-r2_p = stats.pearsonr(test_true, test_pred)
-spearman_r = stats.spearmanr(test_true, test_pred)
-print("rmse: {}\nmae: {}\nr2: {}\nrho: {}".format(rmse, mae, r2_p[0], spearman_r[0]))
-yyplot(test_true, test_pred)
+    yyplot(test_true, test_pred)
 
-pdb_list = list(itertools.chain.from_iterable(test_label))
-df = pd.DataFrame({
-    'PDB': pdb_list,
-    'pKd': list(test_true),
-    'predicted': list(test_pred)
-})
+    pdb_list = list(itertools.chain.from_iterable(test_label))
+    df = pd.DataFrame(dict(PDB=pdb_list, pKd=test_true, predicted=test_pred))
+    df.round(3).to_csv('result.tsv', sep='\t')
 
-df.round(3).to_csv("result.tsv", sep="\t")
+    if args.map_path != '':
+        v_grad = VanillaGrad(th_max=args.map_th_max, th_min=args.map_th_min)
+        v_grad.save_saliency_map(args, n_atom_list, saliency_list, pdb_list)
 
-v_grad = VanillaGrad(th_max=args.map_th_max, th_min=args.map_th_min)
-if args.map_path != '':
-    v_grad.save_saliency_map(args, n_atom_list, saliency_list, pdb_list)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ngpu", '-g', help="number of gpu", type=int, default=1)
+    parser.add_argument("--batch_size", '-B', help="batch_size", type=int, default=8)
+    parser.add_argument("--num_workers", help="number of workers", type=int, default=7)
+    parser.add_argument("--n_graph_layer", help="number of GNN layer", type=int, default=4)
+    parser.add_argument("--d_graph_layer", help="dimension of GNN layer", type=int, default=140)
+    parser.add_argument("--n_FC_layer", help="number of FC layer", type=int, default=4)
+    parser.add_argument("--d_FC_layer", help="dimension of FC layer", type=int, default=128)
+    parser.add_argument("--data_fpath", '-d', help="file path of dude data", type=str, default='data_diff/')
+    parser.add_argument("--save_dir", '-S', help="save directory of model parameter", type=str, default='./save/')
+    parser.add_argument("--initial_mu", help="initial value of mu", type=float, default=4.461085466198279)
+    parser.add_argument("--initial_dev", help="initial value of dev", type=float, default=0.19818493842903845)
+    parser.add_argument("--dropout_rate", '-D', help="dropout_rate", type=float, default=0.3)
+    parser.add_argument("--test_keys", '-t', help="test keys", type=str, default='keys/test_keys.txt')
+    parser.add_argument("--map_path", '-P', help="savepath for saliency map", type=str, default='')
+    parser.add_argument("--map_th_max", help="max th value for saliency map", type=int, default=20)
+    parser.add_argument("--map_th_min", help="min th value for saliency map", type=int, default=-20)
+    parser.add_argument('--dataset_version', '-v', help='dataset version', type=int, default=2)
+    args = parser.parse_args()
+    print(args)
+    if args.dataset_version == 1:
+        import dataset as ds
+    elif args.dataset_version == 2:
+        import dataset2 as ds
+    else:
+        raise Exception('Wrong dataset version')
+    main(args)
+
--- train.py
+++ train.py
@@ -8,232 +8,216 @@ import utils
 from rdkit import Chem
 from rdkit.Chem import QED
 
-now = time.localtime()
-s = "%04d-%02d-%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-print(s)
+def read_keyfile(fname, local=False):
+    keys = []
+    for line in open(fname):
+        it = line.rstrip().split('\t')
+        if local:
+            pdb_code, value = it[0], float(it[1])
+        else:
+            pdb_code, ligand_name, year, value = it[0], it[1], int(it[2]), float(it[3])
+        if os.path.exists(f'cbidata/{pdb_code}'):
+            keys.append((pdb_code, value))
+    return keys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--lr", '-l', help="learning rate", type=float, default=0.0001)
-parser.add_argument("--epoch", '-E', help="epoch", type=int, default=1000)
-parser.add_argument("--ngpu", '-g', help="number of gpu", type=int, default=1)
-parser.add_argument("--batch_size", '-B', help="batch_size", type=int, default=8)
-parser.add_argument("--num_workers", '-n', help="number of workers", type=int, default=0)
-parser.add_argument("--n_graph_layer", help="number of GNN layer", type=int, default=4)
-parser.add_argument("--d_graph_layer", help="dimension of GNN layer", type=int, default=140)
-parser.add_argument("--n_FC_layer", help="number of FC layer", type=int, default=4)
-parser.add_argument("--d_FC_layer", help="dimension of FC layer", type=int, default=128)
-parser.add_argument("--data_fpath", '-d', help="file path of dude data", type=str, default='data_select/')
-parser.add_argument("--save_dir", help="save directory of model parameter", type=str, default='./save/')
-parser.add_argument("--initial_mu", help="initial value of mu", type=float, default=4.461085466198279)
-parser.add_argument("--initial_dev", help="initial value of dev", type=float, default=0.19818493842903845)
-parser.add_argument("--dropout_rate", '-D', help="dropout_rate", type=float, default=0.3)
-parser.add_argument("--train_keys", '-T', help="train keys", type=str, default='keys/train_keys.txt')
-parser.add_argument("--test_keys", '-t', help="test keys", type=str, default='keys/val_keys.txt')
-parser.add_argument('--clear_cache', '-0', help='clear cache directory', action='store_true')
-parser.add_argument('--dataset_version', '-v', help='dataset version', type=int, default=2)
-args = parser.parse_args()
-if args.dataset_version == 1:
-    import dataset as ds
-elif args.dataset_version == 2:
-    import dataset2 as ds
-else:
-    raise Exception('Wrong dataset version')
-
-print(args)
-
-# hyper parameters
-num_epochs = args.epoch
-lr = args.lr
-ngpu = args.ngpu
-batch_size = args.batch_size
-data_fpath = args.data_fpath
-save_dir = args.save_dir
-
-if args.clear_cache:
-    shutil.rmtree('/tmp/moldata')
-
-# make save dir if it doesn't exist
-if not os.path.isdir(save_dir):
-    os.system('mkdir ' + save_dir)
-
-# read data. data is stored in format of dictionary.
-# Each key has information about protein-ligand complex.
-train_keys = []
-train_pkd = []
-for line in open(args.train_keys):
-    it = line.rstrip().split('\t')
-    pdb_code, ligand_name, year, value = it[0], it[1], int(it[2]), float(it[3])
-    train_keys.append(pdb_code)
-    train_pkd.append(value)
-
-test_keys = []
-test_pkd = []
-for line in open(args.test_keys):
-    it = line.rstrip().split('\t')
-    pdb_code, ligand_name, year, value = it[0], it[1], int(it[2]), float(it[3])
-    test_keys.append(pdb_code)
-    test_pkd.append(value)
-
-###############
-### Stratifying
-###############
-all_data = sorted(zip(train_keys + test_keys, train_pkd + test_pkd), key=lambda x: x[1], reverse=True)
-
-train_keys = []
-train_pkd = []
-test_keys = []
-test_pkd = []
-test2_keys = []
-test2_pkd = []
-
-count = 0
-for key, pkd in all_data:
-    if pkd < 2:
-        test2_keys.append(key)
-        test2_pkd.append(pkd)
-        continue
-
+def read_ligand(dirname):
     sdf_found = False
-    for f in os.listdir(f'cbidata/{key}'):
+    for f in os.listdir(dirname):
         if f.endswith('.sdf'):
             sdf_found = True
-            sdf_fname = f'cbidata/{key}/{f}'
-            ligand_mol = Chem.MolFromMolFile(sdf_fname)
+            sdf_fname = f'{dirname}/{f}'
+            for ligand_mol in Chem.SDMolSupplier(sdf_fname):
+                break
             break
-    if not sdf_found:
-        continue
+    return ligand_mol if sdf_found else None
 
-    n_atoms = ligand_mol.GetNumAtoms()
-    if n_atoms < 10 or 45 < n_atoms:
-        test2_keys.append(key)
-        test2_pkd.append(pkd)
-        continue
-    if QED.qed(ligand_mol) < 0.4:
-        test2_keys.append(key)
-        test2_pkd.append(pkd)
-        continue
+def filter_and_stratify(*dataset):
+    all_data = []
+    for _ in dataset:
+        all_data += _
+    data = sorted(all_data, key=lambda x: x[1], reverse=True)
+    train = []
+    test = []
+    test2 = []
 
-    count += 1
+    count = 0
+    for key, pkd in all_data:
+        if pkd < 2:
+            test2.append((key, pkd))
+            continue
 
-    if count % 5 == 0:
-        test_keys.append(key)
-        test_pkd.append(pkd)
+        ligand_mol = read_ligand(f'cbidata/{key}')
+
+        n_atoms = ligand_mol.GetNumAtoms()
+        if n_atoms < 10 or 45 < n_atoms:
+            test2.append((key, pkd))
+            continue
+        if QED.qed(ligand_mol) < 0.4:
+            test2.append((key, pkd))
+            continue
+
+        count += 1
+
+        if count % 5 == 0:
+            test.append((key, pkd))
+        else:
+            train.append((key, pkd))
+    return train, test, test2
+
+def write_keys(keys, oname):
+    with open(oname, 'wt') as out:
+        for key, pkd in keys:
+            print(key, pkd, sep='\t', file=out)
+
+def main(args):
+    import datetime
+    print(datetime.datetime.now().isoformat())
+
+    # hyper parameters
+    num_epochs = args.epoch
+    lr = args.lr
+    ngpu = args.ngpu
+    batch_size = args.batch_size
+    data_fpath = args.data_fpath
+    save_dir = args.save_dir
+
+    if args.clear_cache:
+        shutil.rmtree('/tmp/moldata')
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    train = read_keyfile(args.train_keys)
+    test = read_keyfile(args.test_keys)
+    train_keys, test_keys, test2_keys = filter_and_stratify(train, test)
+
+    write_keys(train_keys, 'train.local.keys')
+    write_keys(test_keys, 'test.local.keys')
+    write_keys(test2_keys, 'test2.local.keys')
+
+    print(f'Number of train data: {len(train_keys)}')
+    print(f'Number of test data: {len(test_keys)}')
+
+    # initialize model
+    device = "cpu"
+    if 0 < args.ngpu:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print(f'CUDA available: {torch.cuda.is_available()} {device}')
+
+    train_dataset = ds.MolDataset([k for k, v in train_keys], [v for k, v in train_keys], args.data_fpath)
+    test_dataset = ds.MolDataset([k for k, v in train_keys], [v for k, v in test_keys], args.data_fpath)
+
+    N_atom_features = train_dataset[0]['H'].shape[1]//2
+    args.N_atom_features = N_atom_features
+
+    train_dataloader = DataLoader(train_dataset, args.batch_size, \
+        shuffle=True, num_workers=args.num_workers, collate_fn=ds.collate_fn)
+    test_dataloader = DataLoader(test_dataset, args.batch_size, \
+        shuffle=True, num_workers=args.num_workers, collate_fn=ds.collate_fn)
+
+    model = gnn(args)
+    print ('number of parameters : ', sum(p.numel() for p in model.parameters() if p.requires_grad))
+
+    model = utils.initialize_model(model, device)
+
+    #optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    #loss function
+    # loss_fn = nn.BCELoss()
+    loss_fn = nn.MSELoss()
+
+    best_train_loss, best_val_loss = np.inf, np.inf
+
+    for epoch in range(num_epochs):
+        st = time.time()
+        #collect losses of each iteration
+        train_losses = [] 
+        test_losses = [] 
+
+        #collect true label of each iteration
+        train_true = []
+        test_true = []
+        
+        #collect predicted label of each iteration
+        train_pred = []
+        test_pred = []
+        
+        model.train()
+        for sample in train_dataloader:
+            model.zero_grad()
+            H, A1, A2, Y, V, keys, _ = sample 
+            H, A1, A2, Y, V = H.to(device), A1.to(device), A2.to(device), Y.to(device), V.to(device)
+            
+            #train neural network
+            pred = model.train_model((H, A1, A2, V))
+
+            loss = loss_fn(pred, Y) 
+            loss.backward()
+            optimizer.step()
+            
+            #collect loss, true label and predicted label
+            train_losses.append(loss.data.cpu().numpy())
+            train_true.append(Y.data.cpu().numpy())
+            train_pred.append(pred.data.cpu().numpy())
+        
+        model.eval()
+        for sample in test_dataloader:
+            model.zero_grad()
+            H, A1, A2, Y, V, keys, _ = sample 
+            H, A1, A2, Y, V = H.to(device), A1.to(device), A2.to(device), Y.to(device), V.to(device)
+            
+            #train neural network
+            pred = model.train_model((H, A1, A2, V))
+
+            loss = loss_fn(pred, Y) 
+            
+            #collect loss, true label and predicted label
+            test_losses.append(loss.data.cpu().numpy())
+            
+        train_losses = np.mean(np.array(train_losses))
+        test_losses = np.mean(np.array(test_losses))
+        end = time.time()
+        lapse = end - st
+
+        best_train_loss = train_losses if train_losses < best_train_loss else best_train_loss
+
+        if test_losses < best_val_loss:
+            best_val_loss = test_losses
+            torch.save(model.state_dict(), f'{save_dir}/save_{epoch}.pt')
+            torch.save(model.state_dict(), f'{save_dir}/model_weights.pt')
+
+        ls = [f'Epoch: {epoch}',
+              f'Lapse: {lapse:.1f}s',
+              f'Losses: ({train_losses:.3f}, {test_losses:.3f})',
+              f'Best: ({best_train_loss:.3f}, {best_val_loss:.3f})']
+        print('\t'.join(ls))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr", '-l', help="learning rate", type=float, default=0.0001)
+    parser.add_argument("--epoch", '-E', help="epoch", type=int, default=1000)
+    parser.add_argument("--ngpu", '-g', help="number of gpu", type=int, default=1)
+    parser.add_argument("--batch_size", '-B', help="batch_size", type=int, default=8)
+    parser.add_argument("--num_workers", '-n', help="number of workers", type=int, default=0)
+    parser.add_argument("--n_graph_layer", help="number of GNN layer", type=int, default=4)
+    parser.add_argument("--d_graph_layer", help="dimension of GNN layer", type=int, default=140)
+    parser.add_argument("--n_FC_layer", help="number of FC layer", type=int, default=4)
+    parser.add_argument("--d_FC_layer", help="dimension of FC layer", type=int, default=128)
+    parser.add_argument("--data_fpath", '-d', help="file path of dude data", type=str, default='data_select/')
+    parser.add_argument("--save_dir", help="save directory of model parameter", type=str, default='./save/')
+    parser.add_argument("--initial_mu", help="initial value of mu", type=float, default=4.461085466198279)
+    parser.add_argument("--initial_dev", help="initial value of dev", type=float, default=0.19818493842903845)
+    parser.add_argument("--dropout_rate", '-D', help="dropout_rate", type=float, default=0.3)
+    parser.add_argument("--train_keys", '-T', help="train keys", type=str, default='keys/train_keys.txt')
+    parser.add_argument("--test_keys", '-t', help="test keys", type=str, default='keys/val_keys.txt')
+    parser.add_argument('--clear_cache', '-0', help='clear cache directory', action='store_true')
+    parser.add_argument('--dataset_version', '-v', help='dataset version', type=int, default=2)
+    args = parser.parse_args()
+    if args.dataset_version == 1:
+        import dataset as ds
+    elif args.dataset_version == 2:
+        import dataset2 as ds
     else:
-        train_keys.append(key)
-        train_pkd.append(pkd)
-
-with open('train.local.key', 'wt') as out:
-    for key, pkd in zip(train_keys, train_pkd):
-        print(key, pkd, sep='\t', file=out)
-
-with open('test.local.key', 'wt') as out:
-    for key, pkd in zip(test_keys, test_pkd):
-        print(key, pkd, sep='\t', file=out)
-
-with open('test2.local.key', 'wt') as out:
-    for key, pkd in zip(test2_keys, test2_pkd):
-        print(key, pkd, sep='\t', file=out)
-
-###############
-###############
-###############
-
-# print simple statistics about dude data and pdbbind data
-print(f'Number of train data: {len(train_keys)}')
-print(f'Number of test data: {len(test_keys)}')
-
-# initialize model
-device = "cpu"
-if 0 < args.ngpu:
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f'CUDA available: {torch.cuda.is_available()} {device}')
-
-
-train_dataset = ds.MolDataset(train_keys, train_pkd, args.data_fpath)
-test_dataset = ds.MolDataset(test_keys, test_pkd, args.data_fpath)
-
-N_atom_features = train_dataset[0]['H'].shape[1]//2
-args.N_atom_features = N_atom_features
-
-train_dataloader = DataLoader(train_dataset, args.batch_size, \
-     shuffle=True, num_workers=args.num_workers, collate_fn=ds.collate_fn)
-test_dataloader = DataLoader(test_dataset, args.batch_size, \
-     shuffle=True, num_workers=args.num_workers, collate_fn=ds.collate_fn)
-
-model = gnn(args)
-print ('number of parameters : ', sum(p.numel() for p in model.parameters() if p.requires_grad))
-
-model = utils.initialize_model(model, device)
-
-#optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-#loss function
-# loss_fn = nn.BCELoss()
-loss_fn = nn.MSELoss()
-
-best_train_loss, best_val_loss = np.inf, np.inf
-
-for epoch in range(num_epochs):
-    st = time.time()
-    #collect losses of each iteration
-    train_losses = [] 
-    test_losses = [] 
-
-    #collect true label of each iteration
-    train_true = []
-    test_true = []
-    
-    #collect predicted label of each iteration
-    train_pred = []
-    test_pred = []
-    
-    model.train()
-    for sample in train_dataloader:
-        model.zero_grad()
-        H, A1, A2, Y, V, keys, _ = sample 
-        H, A1, A2, Y, V = H.to(device), A1.to(device), A2.to(device), Y.to(device), V.to(device)
-        
-        #train neural network
-        pred = model.train_model((H, A1, A2, V))
-
-        loss = loss_fn(pred, Y) 
-        loss.backward()
-        optimizer.step()
-        
-        #collect loss, true label and predicted label
-        train_losses.append(loss.data.cpu().numpy())
-        train_true.append(Y.data.cpu().numpy())
-        train_pred.append(pred.data.cpu().numpy())
-    
-    model.eval()
-    for sample in test_dataloader:
-        model.zero_grad()
-        H, A1, A2, Y, V, keys, _ = sample 
-        H, A1, A2, Y, V = H.to(device), A1.to(device), A2.to(device), Y.to(device), V.to(device)
-        
-        #train neural network
-        pred = model.train_model((H, A1, A2, V))
-
-        loss = loss_fn(pred, Y) 
-        
-        #collect loss, true label and predicted label
-        test_losses.append(loss.data.cpu().numpy())
-        
-    train_losses = np.mean(np.array(train_losses))
-    test_losses = np.mean(np.array(test_losses))
-    end = time.time()
-    lapse = end - st
-    best_train_loss = train_losses if train_losses < best_train_loss else best_train_loss
-
-    if test_losses < best_val_loss:
-        best_val_loss = test_losses
-
-        # ベスト値のときのみ保存
-        name = save_dir + '/save_' + str(epoch) + '.pt'
-        torch.save(model.state_dict(), name)
-
-        name = save_dir + '/model_weights.pt'
-        torch.save(model.state_dict(), name)
-
-    print(f"Epoch: {epoch}\t{lapse:.1f}s\ttrain loss: {train_losses:.3f}\tval loss: {test_losses:.3f}\ttrain best loss: {best_train_loss:.3f}\tval best loss: {best_val_loss:.3f}")
+        raise Exception('Wrong dataset version')
+    print(args)
+    main(args)
